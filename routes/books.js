237670abbc649c44c9db6459a332f33360eee6c4
ssmeds <stina_smeds@hotmail.com>
2021-04-15 13:07:32 +0200

    created a json-file with the books and added fs-logic

--- routes/books.js
+++ routes/books.js
@@ -1,55 +1,34 @@
 var express = require('express');
 var router = express.Router();
+let fs = require("fs");
 
-const books = [{
-    title: "Låt den rätta komma in",
-    author: "John Ajvide-Lindqvist",
-    lent: false,
-    id: 1
-  },
-  {
-    title: "Människohamn",
-    author: "John Ajvide-Lindqvist",
-    lent: false,
-    id: 2
-  },
-  {
-    title: "Pappersväggar",
-    author: "John Ajvide-Lindqvist",
-    lent: false,
-    id: 3
-  },
-  {
-    title: "Lilla stjärna",
-    author: "John Ajvide-Lindqvist",
-    lent: true,
-    id: 4
-  },
-  {
-    title: "Hantering av odöda",
-    author: "John Ajvide-Lindqvist",
-    lent: false,
-    id: 5
-  }
-];
+const booksJson = './books.json';
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
-  let printBooks = `<div><h1>Mina böcker</h1>`;
-  let text;
-
-  for (book in books) {
-    if (books[book].lent == true) {
-      text = "Ja"
-    } else {
-      text = "Nej"
+  fs.readFile(booksJson, (err, data) => {
+    if (err) {
+      console.log(err);
     }
-    printBooks += `<div><ul><p id="${books[book].id}"><a href="books/info/:${books[book].id}"><li>Titel: ${books[book].title}</li></a></p> <p><li>Utlånad: ${text}</li></p><br></ul></div>`
-  }
-  printBooks += `</div><div><a href="/books/add"> Lägg till en ny bok </a></div>`;
-  // console.log(req.params);
-  res.send(printBooks);
+    let books = JSON.parse(data);
+    let printBooks = `<div><h1>Mina böcker</h1>`;
+    let text;
+
+    for (book in books) {
+      if (books[book].lent == true) {
+        text = "Ja"
+      } else {
+        text = "Nej"
+      }
+      printBooks += `<div><ul><p id="${books[book].id}"><a href="books/info/:${books[book].id}"><li>Titel: ${books[book].title}</li></a></p> <p><li>Utlånad: ${text}</li></p><br></ul></div>`
+    }
+    printBooks += `</div><div><a href="/books/add"> Lägg till en ny bok </a></div>`;
+    // console.log(req.params);
+    res.send(printBooks);
+  })
+
 });
 
 router.get('/add', function (req, res) {
@@ -69,60 +48,98 @@ router.get('/add', function (req, res) {
 
 router.post("/add", function (req, res) {
   // console.log(req.body);
+  fs.readFile(booksJson, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    let books = JSON.parse(data);
+    let newBookId = books.length + 1;
 
-  let newBookId = books.length + 1;
+    let newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      lent: true,
+      id: newBookId
+    };
+    books.push(newBook);
+    fs.writeFile(booksJson, JSON.stringify(books, null, 2), (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("bok tillagd");
+    })
+    res.redirect("/books")
 
-  let newBook = {
-    title: req.body.title,
-    author: req.body.author,
-    lent: true,
-    id: newBookId
-  };
-  books.push(newBook);
 
-  res.redirect("/books")
-
-})
-
+  })
+});
 
 router.get('/info/:bookId', function (req, res) {
   let bookInfo = `<div><h2>Information om bok</h2>`;
   // console.log(req.params.bookId);
   let text;
-  for (book in books) {
-    if (req.params.bookId == ":" + books[book].id) {
-      if (books[book].lent == true) {
-        text = "Ja";
-      } else {
-        text = "Nej"
-      };
-
-      bookInfo += `<div>Titel: ${books[book].title}</div>
-                  <div>Författare: ${books[book].author}</div>
-                  <div>Utlånad: ${text}</div>
-                  <div><a href="/books/lend/:${books[book].id}">Låna eller lämna tillbaka boken här</a></div>
-      </div>`
+  fs.readFile(booksJson, (err, data) => {
+    if (err) {
+      console.log(err);
     }
-  }
+    let books = JSON.parse(data);
+    for (book in books) {
+      if (req.params.bookId == ":" + books[book].id) {
+        if (books[book].lent == true) {
+          text = "Ja";
+        } else {
+          text = "Nej"
+        };
 
-  // console.log(req);
-  res.send(bookInfo);
+        bookInfo += `<div>Titel: ${books[book].title}</div>
+                    <div>Författare: ${books[book].author}</div>
+                    <div>Utlånad: ${text}</div>
+                    <div><a href="/books/lend/:${books[book].id}">Låna eller lämna tillbaka boken här</a></div>
+        </div>`
+      }
+    }
+
+    // console.log(req);
+    res.send(bookInfo);
+  })
+
 })
 
 router.get('/lend/:bookId', function (req, res) {
   console.log("från lend", req.params.bookId);
-  for (book in books) {
-    if (req.params.bookId == ":" + books[book].id) {
-      console.log(books[book].title);
-      if (books[book].lent == true) {
-        books[book].lent = false
-      } else {
-        books[book].lent = true;
+
+  fs.readFile(booksJson, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    let books = JSON.parse(data);
+    for (book in books) {
+      if (req.params.bookId == ":" + books[book].id) {
+        console.log(books[book].title);
+        if (books[book].lent == true) {
+          books[book].lent = false;
+          fs.writeFile(booksJson, JSON.stringify(books, null, 2), (err) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log("books updated");
+          })
+        } else {
+          books[book].lent = true;
+          fs.writeFile(booksJson, JSON.stringify(books, null, 2), (err) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log("books updated");
+          })
+        }
+
       }
     }
-  }
-  // console.log(books);
-  res.redirect("/books");
+    // console.log(books);
+    res.redirect("/books");
+  })
+
 })
 
 module.exports = router;
